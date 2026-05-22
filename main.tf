eadec59daaa7976ffa2c5988bf6f31d4bb59467f
stodmis <stodmis@gmail.com>
2026-05-22 14:33:57 +0200

    Added a security group and security group rules for http https traffic.

--- main.tf
+++ main.tf
@@ -15,10 +15,54 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_instance" "blog" {
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow HTTP and HTTPS inbound traffic. Allow every outbound traffic."
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  from_port         = 443
+  protocol          = "tcp"
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  from_port         = 0
+  protocol          = "-1"
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
