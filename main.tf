ee21f4f94d8b43f04eafcc3756fddd5627db8b01
dmytro.stolbovyi <dmytro.stolbovyi@globallogic.com>
2026-07-29 17:38:40 +0200

    Added security_group module

--- main.tf
+++ main.tf
@@ -22,10 +22,39 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.id]
 
   tags = {
     Name = "HelloWorld!"
+  }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "6.0.0"
+  name    = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules = {
+    https = {
+      from_port   = 443
+      ip_protocol = "tcp"
+      cidr_ipv4   = "10.0.0.0/16"
+      description = "HTTPS from internal"
+    }
+    self-all = {
+      ip_protocol                  = "-1"
+      referenced_security_group_id = "self"
+      description                  = "All traffic from members of this SG"
+    }
+  }
+
+  egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
   }
 }
 
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+
+output "security_group_arn" {
+  value = module.blog_sg.arn
+}
