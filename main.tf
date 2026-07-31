9dc289d85a1ac1f7e00731291d707091226c322e
dmytro.stolbovyi <dmytro.stolbovyi@globallogic.com>
2026-07-31 11:12:47 +0200

    Added the ami implicitly to the instance

--- main.tf
+++ main.tf
@@ -1,25 +1,9 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 data "aws_vpc" "default" {
   default = true
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = ami-02b64aa047cb5edf5
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.id]
