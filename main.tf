af360d6c87c4fe21e691131be02047c88d18052a
dmytro.stolbovyi <dmytro.stolbovyi@globallogic.com>
2026-07-31 14:53:45 +0200

    Fixed a network prefix

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "blog_vpc" {
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["${var.environment.network_prefix}.1.0/24", "${var.environment.network_prefix}.2.0/24", "${var.environment.network_prefix}.3.0/24"]
-  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "10.${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
