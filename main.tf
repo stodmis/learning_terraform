082f73358169e699fa1318f78498c018ea3abcca
dmytro.stolbovyi <dmytro.stolbovyi@globallogic.com>
2026-07-31 11:29:40 +0200

    Added a new VPC

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
-data "aws_vpc" "default" {
-  default = true
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -7,6 +22,8 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld!"
@@ -18,7 +35,7 @@ module "blog_sg" {
   version = "6.0.0"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = {
     https = {
